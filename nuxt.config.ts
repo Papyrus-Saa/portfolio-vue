42111c3c679e4d1fd76e19f52eb0a4d67b802263
RSBrand <saavedra.ramon.brand@gmail.com>
2025-01-03 13:51:54 +0100

    New update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,9 @@
-import Aura from "@primevue/themes/aura";
-
 export default defineNuxtConfig({
   alias: {
     "@": "../my-portfolio",
   },
   devtools: { enabled: true },
   pages: true,
-
-  modules: ["@primevue/nuxt-module"],
 
   css: ["~/assets/css/main.css"],
 
